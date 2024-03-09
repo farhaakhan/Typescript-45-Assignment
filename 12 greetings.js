9844b8a344e9e9b11c4984153e335e5068a86282
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 11:53:15 +0500

    Add files via upload

---
+++ 12 greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let members = ['saima', 'farhana', 'fouzia', 'laiba',];
+let message = 'tomorow will be bithday my friend: ';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
