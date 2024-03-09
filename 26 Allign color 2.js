02a2e6787ca6e6609221e4eab8467ccec5cf14f4
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 12:26:17 +0500

    Add files via upload

---
+++ 26 Allign color 2.js
@@ -0,0 +1,19 @@
+//  Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+var alienColor = "GREEN";
+// write an if-else chain.
+// If the alien’s color is green,
+//print  the player just earned 5 points for shooting the alien.
+if (alienColor === "GREEN") {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
+//• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+alienColor = "yellow";
+if (alienColor === "GREEN") {
+    console.log("the player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("the player just earned 10 points.");
+}
