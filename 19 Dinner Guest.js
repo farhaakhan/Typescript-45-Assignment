89654865a0ee1109358b643199e5a11fe355243b
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 12:07:51 +0500

    Add files via upload

---
+++ 19 Dinner Guest.js
@@ -0,0 +1,6 @@
+"use strict";
+// 19 EXCERCISE Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_1 = require("../14 Guest List/app");
+console.log(" n PRINTING NUMBER OF GUEST INVITED");
+console.log("we had finally invited ".concat(app_1.guest_list.length, " from excercise 14"));
