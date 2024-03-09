e65a6f40aac5659c1a12a1078b75eceb6b6e0ebb
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 11:56:46 +0500

    Add files via upload

---
+++ 14 guest list.js
@@ -0,0 +1,9 @@
+"use strict";
+// guest list
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+var guest_list = ['farha', 'fouzia', 'laiba', 'areeba'];
+exports.guest_list = guest_list;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log(' \n Respected sir/Madam ' + guest_list[i] + ',\nwe invited you on dinner tomorow.\n\nThank you');
+}
