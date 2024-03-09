f4b6661c23d0ce8ffbade19f26fe8bcc8c5b8278
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 12:10:40 +0500

    Add files via upload

---
+++ 20 think of something you could store in array.js
@@ -0,0 +1,7 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items
+var languages = ['URDU ', 'ENGLISH ', 'ARABIC ', 'FARSI ', 'HINDI ',];
+console.log("list of languages:");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
