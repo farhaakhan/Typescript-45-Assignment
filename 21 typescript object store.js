7401adff719770919a412311bfab9fa2b4b10c90
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 12:12:29 +0500

    Add files via upload

---
+++ 21 typescript object store.js
@@ -0,0 +1,14 @@
+// They think of something you could store in a TypeScript Object. 
+// Write a program that creates Objects containing these item
+
+
+var book = {
+    name: 'Typescript python',
+    Price: 5000
+};
+var watch = {
+    name: 'RADO SMART WATCH',
+    Price: 2000
+};
+console.log("book name: ".concat(book.name, ", price: $").concat(book.Price, " "));
+console.log("watch name:  ".concat(watch.name, " , Price:  $").concat(watch.Price));
