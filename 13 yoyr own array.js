372f85ac664025512ba1697d5961d679a2571a37
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 11:55:07 +0500

    Add files via upload

---
+++ 13 yoyr own array.js
@@ -0,0 +1,6 @@
+"use strict";
+// excersie 13 transportaion list 
+let transportaion = ['civic', 'bike', 'bus', 'suzuki', 'coach',];
+for (let i = 0; i < transportaion.length; i++) {
+    console.log(`i would like to own a ` + transportaion[i]);
+}
