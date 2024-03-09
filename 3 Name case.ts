f09f3fdb14f9c5b05f8ce851ace1c3e4c9bcc283
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 11:33:29 +0500

    Add files via upload

---
+++ 3 Name case.ts
@@ -0,0 +1,15 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, 
+// “Hello Eric, would you like to learn some Python today?”
+
+
+let personName: string = "farhana";
+
+// Convert to lowercase
+console.log("Lowercase:", personName.toLowerCase());
+
+// Convert to uppercase
+console.log("Uppercase:", personName.toUpperCase());
+
+// Convert to titlecase
+console.log("Titlecase:", personName.replace(/\b\w/g, c => c.toUpperCase()));
+
