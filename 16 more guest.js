afed2c3f77172a71aa9aacda4d4a132b2836917e
farhaakhan <162781631+farhaakhan@users.noreply.github.com>
2024-03-09 12:00:55 +0500

    Add files via upload

---
+++ 16 more guest.js
@@ -0,0 +1,18 @@
+var guest_list = ['farha', 'fouzia', 'laiba', 'areeba'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/Madam . ' + guest_list[i] + ',\n\nnow invited you on dinner tomorow.\n\nthank you!\n\n\ ');
+}
+var not_present = 'fouzia';
+var new_guest = 'talat';
+guest_list[1] = new_guest;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/Madam .' + guest_list[i] + ',\n\nnow invited you on dinner tomorow. \n\nthank you!\n\n\ ');
+}
+console.log("mis. ".concat(not_present, " will not coming tomorow at dinner party"));
+console.log('Good News! we find big table so we are inviting more 3 guest.');
+guest_list.unshift('Asma');
+guest_list.splice(2, 0, 'Reahana');
+guest_list.push('yasmeen');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/Madam .' + guest_list[i] + ',\n\nnow invited you on dinner tomorow. \n\nthank you!\n\n\ ');
+}
